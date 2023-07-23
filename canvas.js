87a75f5eb53a5f1a0a281572f6a773a6c1fd489a
Kolamin <gofranich@gmail.com>
2023-07-23 07:03:53 +0600

    finish animate in canvas

--- canvas.js
+++ canvas.js
@@ -32,50 +32,56 @@ c.stroke() */ // Arc / Circle
   c.stroke();
 } */
 
-
 class Circle {
-  constructor(x, y) {
+  constructor(x, y, dx, dy, radius) {
     this.x = x;
     this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
   }
 
-    draw(){
-      c.beginPath();
-      c.arc(this.x, this.y, 30, 0, Math.PI * 2, false);
-      c.strokeStyle = "blue";
-      c.stroke();
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.strokeStyle = "blue";
+    c.stroke();
+  }
+
+  update() {
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
     }
+
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+
+    this.x += this.dx;
+    this.y += this.dy;
+
+    this.draw();
+  }
 }
 
-let circle = new Circle(200, 200);
-circle.draw()
-
-let x = Math.random() * innerWidth;
-let y = Math.random() * innerHeight;
-let dx = (Math.random() - 0.5) * 8;
-let dy = (Math.random() - 0.5) * 8;
-let radius = 30;
+let circleArray = [];
+for (let i = 0; i < 100; i++) {
+  let radius = 30;
+  let x = Math.random() * (innerWidth - radius * 2) + radius;
+  let y = Math.random() * (innerWidth - radius * 2) + radius;
+  let dx = Math.random() - 0.5;
+  let dy = Math.random() - 0.5;
+  
+  circleArray.push(new Circle(x, y, dx, dy, radius));
+}
 
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, innerWidth, innerHeight);
 
-  circle.draw();
-  c.beginPath();
-  c.arc(x, y, 30, 0, Math.PI * 2, false);
-  c.strokeStyle = "blue";
-  c.stroke();
-
-  if (x + radius > innerWidth || x - radius < 0) {
-    dx = -dx;
+  for (let i = 0; i < circleArray.length; i++) {
+    circleArray[i].update();
   }
-
-  if (y + radius > innerHeight || y - radius < 0) {
-    dy = -dy;
-  }
-
-  x += dx;
-  y += dy;
 }
 
 animate();
